5b8f4c7cd2f618ea26caac272449f08df756c9dd
Valerius Mattfeld <10323945+valerius21@users.noreply.github.com>
2021-05-16 13:59:52 +0200

    add image feed

--- pages/pick.tsx
+++ pages/pick.tsx
@@ -1,13 +1,40 @@
 import React from 'react'
+import Image from 'next/image'
+import ArrowBtn from '../components/ArrowButton'
+import styles from '../styles/Pick.module.css'
 
 const UID = 'USER_ID_XXX'
 
 const Pick = () => {
     return (
-        <div>
-            <div>{UID}</div>
-            <div>Tap a picture to get started!</div>
-            {/* TODO: add picture list */}
+        <div className="page">
+            <div className="grid grid-cols-3 gap-4 mt-4">
+                <ArrowBtn />
+                <h3 className={styles.uid}>{UID}</h3>
+                <div />
+                <div className={styles.rect}>
+                    <div className={styles.tap}>Tap a picture to get started! 😊</div>
+                </div>
+                <div className={styles.feed}>
+                    <img
+                        className={styles.pictureCard}
+                        src='/stock_1.jpg'
+                    />
+                    <br />
+                    <img
+                        className={styles.pictureCard}
+                        src='/stock_2.jpg'
+                    />
+                    <br />
+                    <img
+                        className={styles.pictureCard}
+                        src='/stock_3.jpg'
+                    />
+                    <br />
+                    {/* TODO: add picture list */}
+
+                </div>
+            </div>
         </div>
     )
 }
