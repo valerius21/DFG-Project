cb4e3a44a2fa7898bd36436f4cb40d39d1a34a9f
Valerius Mattfeld <valerius.mattfeld@uni-goettingen.de>
2021-06-21 15:22:38 +0200

    setting up CI

--- next.config.js
+++ next.config.js
@@ -9,5 +9,6 @@ module.exports = {
     // }
     images: {
         domains: ['live.staticflickr.com', 'images.unsplash.com']
-    }
+    },
+    assetPrefix: process.env.NODE_ENV === 'production' ? '/dfg-website' : '',
 }
