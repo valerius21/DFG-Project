0148e4409d424cdec02c94ba6803fe2826fa07fa
Valerius <10323945+valerius21@users.noreply.github.com>
2021-06-21 16:08:41 +0200

    change next.config.js

--- next.config.js
+++ next.config.js
@@ -1,14 +1,5 @@
 module.exports = {
-    // webpackDevMiddleware: (config) => {
-    //     // Solve compiling problem via vagrant
-    //     config.watchOptions = {
-    //         poll: 1000,   // Check for changes every second
-    //         aggregateTimeout: 300,   // delay before rebuilding
-    //     };
-    //     return config;
-    // }
     images: {
         domains: ['live.staticflickr.com', 'images.unsplash.com']
     },
-    assetPrefix: process.env.NODE_ENV === 'production' ? '/dfg-website' : '',
 }
